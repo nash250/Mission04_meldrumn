b86ad1c3841c0a70221a24e42d21e58aefc01f79
meldrumn <nashmeldrum@gmail.com>
2023-02-06 16:56:41 -0700

    checkpoint 5 update

---
+++ MyFirstASPNETWEBAPP/Controllers/BlahController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstASPNETWEBAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Controller Program
+namespace MyFirstASPNETWEBAPP.Controllers
+{
+    // Public class for controller
+    public class BlahController : Controller
+    {
+        // Controller to return view for index page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Get method for Calc Page
+        [HttpGet]
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+
+        // Post method for Calc Page
+        [HttpPost]
+        public IActionResult GradeCalc (GradeCalcModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MyFirstASPNETWEBAPP/Models/GradeCalcModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Model for Grade Calculator
+namespace MyFirstASPNETWEBAPP.Models
+{
+    public class GradeCalcModel
+    {
+        // Data Validation for Inputs in Calculator
+        [Required]
+        [Range(0,100, ErrorMessage = "Please Enter a Number Between 0 and 100")]
+        public int atext { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please Enter a Number Between 0 and 100")]
+        public int gtext { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please Enter a Number Between 0 and 100")]
+        public int qtext { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please Enter a Number Between 0 and 100")]
+        public int mtext { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please Enter a Number Between 0 and 100")]
+        public int ftext { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please Enter a Number Between 0 and 100")]
+        public int itext { get; set; }
+    }
+}
